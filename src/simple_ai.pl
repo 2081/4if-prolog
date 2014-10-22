39740d0ed6291f18cc5d2027fe34804e6e61250f
LilCartell <lilian.cartellier01@orange.fr>
2014-10-22 22:18:56 +0200

    Comment on greedy and simple

--- src/simple_ai.pl
+++ src/simple_ai.pl
@@ -1,19 +1,25 @@
 :- dynamic ai_play/1 .
 
+
+/* Recherche le coup à jouer permettant de marquer autant de point que Score */
 simple_ai(Line,Score) :-
 	free_lines(Lines),
 	element(Line,Lines,_),
 	close_count(Line,Score).
 	
+/* L'ai cherche à remplir 2 carrés */
 simple_ai(Line) :-
 	simple_ai(Line,2),!.
+/*Si ce n'est pas possible, l'ai cherche à remplir 1 carré */
 simple_ai(Line) :-
 	simple_ai(Line,1),!.
+/*Si ce n'est pas possible, l'ai cherche à jouer un coup aléatoire qui ne permet pas à l'opposant de fermer un carré */
 simple_ai(Line) :-
 	findall(L,(simple_ai(L,0),not(critical(L))),Lines),
 	length(Lines,N),
 	random(1,N,R),
 	nth1(R,Lines,Line),!.
+/* En dernier recours, l'ai joue tout simplement. */
 simple_ai(Line) :-
 	simple_ai(Line,0),!.
 
